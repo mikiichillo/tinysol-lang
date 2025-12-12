07b86540de64c45d177f77dfe5df1b09550b4249
Massimo Bartoletti <bart@unica.it>
2025-12-12 15:12:43 +0100

    added unit tests for short-circuit semantics of boolean operators

--- test/semantics.ml
+++ test/semantics.ml
@@ -350,6 +350,41 @@ let%test "test_block_2" = test_exec_tx
   ["0xA:0xC.f(1)"] 
   ["x==100 && y==200 && z==5"]
 
+let%test "test_shortcut_1" = test_exec_tx
+  "contract C {  
+      uint x;
+      function f() public { if (x==1 || this.g()==1) x+=1; else x=5; }
+      function g() public returns(uint) { require(x==0); return 1; } 
+  }"
+  ["0xA:0xC.f()"] 
+  [("x==1");]
+
+let%test "test_shortcut_2" = test_exec_tx
+  "contract C {  
+      uint x;
+      function f() public { if (x==1 || this.g()==1) x+=1; else x=5; }
+      function g() public returns(uint) { require(x==0); return 1; } 
+  }"
+  ["0xA:0xC.f()"; "0xA:0xC.f()"] 
+  [("x==2");]
+
+let%test "test_shortcut_3" = test_exec_tx
+  "contract C {  
+      uint x;
+      function f() public { if (x==0 && this.g()==1) x=1; else x=5; }
+      function g() public returns(uint) { require(x==0); return 1; }
+  }"
+  ["0xA:0xC.f()"] 
+  [("x==1");]
+
+let%test "test_shortcut_4" = test_exec_tx
+  "contract C {  
+      uint x;
+      function f() public { if (x==0 && this.g()==1) x=1; else x=5; }
+      function g() public returns(uint) { require(x==0); return 1; }
+  }"
+  ["0xA:0xC.f()"; "0xA:0xC.f()"] 
+  [("x==5");]
 
 (********************************************************************************
  test_exec_fun : 
